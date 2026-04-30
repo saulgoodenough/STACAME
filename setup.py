f235c95a6dd5f6af2081d898fe1137dd4bbe6e51
Saul Goodenough <littlebiao@outlook.com>
2026-04-30 19:03:07 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import Command, find_packages, setup
 __lib_name__ = "STACAME"
 __lib_version__ = "1.0.0"
 __description__ = "Integrating spatial transcriptomics data across species"
-__url__ = "https://github.com/saulgoodenough/STASAGE"
+__url__ = "https://github.com/zhanglabtools/STACAME"
 __author__ = "Biao Zhang"
-__author_email__ = "zhangb20@fudan.edu.cn"
+__author_email__ = "biaozhang@ysu.edu.cn"
 __license__ = "MIT"
 __keywords__ = ["spatial transcriptomics", "Cross-species", "Brain", "data integration", "Graph attention auto-encoder", "spatial domain", "three-dimensional reconstruction"]
 __requires__ = ["requests",]
@@ -18,7 +18,7 @@ setup(
     author = __author__,
     author_email = __author_email__,
     license = __license__,
-    packages = ['STASAGE'],
+    packages = ['STACAME'],
     install_requires = __requires__,
     zip_safe = False,
     include_package_data = True,
